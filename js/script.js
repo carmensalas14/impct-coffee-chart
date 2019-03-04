338abbd49e8a6f5fa017363ab6e678a6af526276
Carmen Salas <clivingrooms@aol.com>
2019-03-04 14:40:55 +0000

    added logo changed chart colors added data and styled font

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 /* global Highcharts */
 /* global $ */
+var coffeeConsumed = 344;
+var educationHours = coffeeConsumed * .06;
+
 $(function () {
+    Highcharts.setOptions({
+        chart: {
+            backgroundColor: null
+        },
+        colors: ['#a25ea5', '#a8d15c', '#80f6fc'],
+    
+    });
   
     Highcharts.chart('chartContainer', {
 
         chart: {
             type: 'solidgauge',
         
-        marginTop: 10,
-            backgroundColor: null
+        marginTop: 50,
+            
         },
 
         title: {
@@ -17,8 +27,9 @@ $(function () {
                 fontSize: '24px'
             }
         },
+        
         subtitle: {
-          text: '<div style="text-align:center"><p style="float:left;width:100%;"><span style="font-size:15px; color:#571e4f"> Hours </span><br><span style="font-size:15px; color:#571e4f"> lbs of coffee consumed</span></p></div>',
+          text: '<div style="text-align:center"><p style="float:left;width:100%;"><span style="font-size:16px; font-weight:800; font-family:Montserrat, Helvetica sans-serif; color:#915591">344 lbs of coffee </span><br><span style="font-size:16px; font-weight:800; font-family:Montserrat, Helvetica sans-serif; color:#915591">consumed</span><br><br><br><span style="font-size:16px; font-weight:800; font-family:Montserrat, Helvetica sans-serif; color:#799929"> 206.4 hours of </span><br><span style="font-size:16px; font-weight:800; font-family:Montserrat, Helvetica sans-serif; color:#799929">education created</span></p></div>',
           x: 0,
           y: 170
         },
@@ -26,26 +37,26 @@ $(function () {
         enabled: false
     },
 
-        
+// Tracks for circles
         pane: {
             startAngle: 0,
             endAngle: 360,
-            background: [{ // Track for Move
+            background: [{ // Track for purple hours
                 outerRadius: '112%',
                 innerRadius: '88%',
-                backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.3).get(),
+                backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.7).get(),
                 borderWidth: 0
-            }, { // Track for Exercise
+            }, { // Track for green coffee 
                 outerRadius: '87%',
                 innerRadius: '63%',
-                backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0.3).get(),
+                backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0.6).get(),
                 borderWidth: 0
             },
-             { // Track for Stand
-            outerRadius: '62%',
+             { // Center of chart background color
+            outerRadius: '63%',
             innerRadius: '0%',
             backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[2])
-                .setOpacity(0.6)
+                .setOpacity(0.8)
                 .get(),
             borderWidth: 0
             }]
@@ -53,7 +64,7 @@ $(function () {
 
         yAxis: {
             min: 0,
-            max: 100,
+            max: 4000,
             lineWidth: 0,
             tickPositions: []
         },
@@ -73,25 +84,23 @@ $(function () {
         },
 
         series: [{
-            name: 'Move',
+            name: 'CoffeeConsumed',
             borderColor: Highcharts.getOptions().colors[0],
             data: [{
                 color: Highcharts.getOptions().colors[0],
                 radius: '100%',
                 innerRadius: '100%',
-                y: 80
+                y: coffeeConsumed
             }]
         }, {
-            name: 'Exercise',
+            name: 'HoursOfEducation',
             borderColor: Highcharts.getOptions().colors[1],
             data: [{
                 color: Highcharts.getOptions().colors[1],
                 radius: '75%',
                 innerRadius: '75%',
-                y: 65
+                y: educationHours
             }]
-        }, {
-            borderColor: Highcharts.getOptions().colors[2]
         }]
     },
 
